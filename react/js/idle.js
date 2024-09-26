646d471c85b745ede620708f9cd9699a66c5148d
caogy <caoguangyao@yunzhangfang.com>
2024-09-26 15:26:56 +0800

    feat: 添加注释

--- react/js/idle.js
+++ react/js/idle.js
@@ -1,16 +1,22 @@
 // react 简易版调度器
 
-// 任务优先级 枚举常量
+// 任务优先级 枚举常量 用于计算超时时间
 
 const ImmediatePriority = 1,
   UserBlockingPriority = 2,
   NormalPriority = 3,
   LowPriority = 4,
   IdlePriority = 5;
-  
+
+/**
+ * 获取当前精确时间 精度最高可达 微秒
+ * 此函数使用 performance.now() 方法获取当前时间
+ * @returns {number} 当前时间的毫秒表示
+ */
 function getCurrentTime() {
-  return performance.now(); // 精确度更高 到 微秒
+  return performance.now(); 
 }
+
 
 class SimpleScheduler {
   constructor() {
@@ -23,8 +29,16 @@ class SimpleScheduler {
     // 接收信息 将任务 推入 队列 这是一个宏任务 下一次执行
     channel.port1.onmessage = this.performWorkUntilDeadline.bind(this);
   }
-  // 调度任务
-  scheduleCallback(priortyLevel, callback){
+
+/**
+ * 将回调函数安排到任务队列中，并根据优先级设置过期时间。
+ * 如果当前时间超过过期时间，则回调函数将被视为过期。
+ *
+ * @param {number} priortyLevel - 回调函数的优先级级别。
+ * @param {function} callback - 要安排的回调函数。
+ * @returns {void}
+ */
+  scheduleCallback(priortyLevel, callback) {
     const curTime = getCurrentTime();
     let timeout
 
@@ -62,9 +76,22 @@ class SimpleScheduler {
     this.schedulePerformWorkUntilDeadline()
 
   }
+
+  /**
+   * 调度执行工作直到截止日期 -- 未实现
+   * 如果当前没有执行工作，则通知相关方 启动workloop。
+   * 此函数用于启动或继续执行任务队列中的工作，直到达到截止日期 时间限制为实现
+   * 
+   * @remarks
+   * 该函数通过检查 `isPerformingWork` 标志来确定是否当前正在执行工作。
+   * 如果没有执行工作，它会设置标志并通过 `postMessage` 方法通知相关方。
+   * 启动后 相当于将一个宏任务 添加到 系统， 在一个 workloop 循环中 一直到结束
+   * 
+   * @returns {void} 该函数不返回任何值。
+   */
   schedulePerformWorkUntilDeadline() {
     // 无任务执行时 通知
-    if(!this.isPerformingWork) {
+    if (!this.isPerformingWork) {
       // this.isPerformingWork = true;
       this.port.postMessage(null) // 
     }
@@ -79,9 +106,9 @@ class SimpleScheduler {
   // 任务循环
   workLoop() {
     let curTask = this.peek(this.taskQueue)
-    while(curTask !== null){
+    while (curTask !== null) {
       const callback = curTask.callback;
-      if(typeof callback === 'function') {
+      if (typeof callback === 'function') {
         callback()
       }
       // 移除已完成的任务
@@ -90,16 +117,16 @@ class SimpleScheduler {
     }
   }
   //获取队列中任务
-  peek(queue){
+  peek(queue) {
     return queue[0] || null
   }
-  pop(queue){
+  pop(queue) {
     return queue.shift()
   }
   push(queue, task) {
     queue.push(task)
     // 对 队列按优先级进行排序 -
-    queue.sort((a,b) => a.expirationTime - b.expirationTime);
+    queue.sort((a, b) => a.expirationTime - b.expirationTime);
   }
 }
 
