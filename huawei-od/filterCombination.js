097ff4c9e99007d03c0d2238038ddd8387786d57
caogy <caoguangyao@yunzhangfang.com>
2024-11-23 11:30:26 +0800

    feat: 补充文档

--- huawei-od/filterCombination.js
+++ huawei-od/filterCombination.js
@@ -38,3 +38,28 @@ function filterCombination(n, powers, maxPower) {
 
 // filterCombination(4, [50, 20, 20, 60], 90);
 filterCombination(3, [2, 3, 10], 9);
+
+// 代码二 这个好理解
+function findOptimalPower (n, powers, maxPower) {
+  // 初始化dp数组 dp[i]表示功率是否可达
+  const dp = new Array(maxPower + 1).fill(false);
+  dp[0] = true; // 功率0，总是可达的
+  // 遍历每一个充电设备的功率
+  for (let power of powers) {
+    // 从最大的功率开始，逐个递减 从后向前更新功率
+    for (let i = maxPower; i >= power; i-- ) {
+      if (dp[i - power]) {
+        dp[i] = true;
+      }
+    }
+  }
+  // 寻找最接近 pMax 但不超过 pMax 的功率值
+  for (let i = maxPower; i >= 0; i--) {
+    if (dp[i]) {
+      return i;
+    }
+  }
+  return 0;
+}
+
+console.log(findOptimalPower(3, [2,3, 10], 9));
