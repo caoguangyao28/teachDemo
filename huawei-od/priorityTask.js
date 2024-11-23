f4fbb3ad6a262ba2bf54136407e09091e58643e5
caoguangyao <caoguangyao28@163.com>
2024-11-23 22:05:02 +0800

    feat: 初始化任务依赖解析执行顺序

--- huawei-od/priorityTask.js
+++ huawei-od/priorityTask.js
@@ -46,35 +46,54 @@
 //   process.exit()
 // })();
 
+function getSets(inputs) {
+  // 依赖其他任务的集合
+  const set1 = new Set(); // 依赖他人的集合
+  const set2 = new Set(); // 被他人依赖的集合
+  for (let i = 0; i < inputs.length; i++) {
+    const item = inputs[i];
+    const [yilai, beiyilai] = item.split("->");
+    set1.add(yilai);
+    set2.add(beiyilai);
+  }
+  // 只存在 set2 不存在 set1 中的元素 可以立即执行的任务
+  const valueSet = new Set([...set2].filter(item => !set1.has(item)));
+  // valueSet 转数组并 排序，这个也是 最优先执行的任务
+  const startPoints = [...valueSet].sort();
+  return [startPoints];
+}
+
 // 循环逐一追加法
 function appendTo(inputs) {
+  inputs.sort((a, b) => a.localeCompare(b));
   const res = [];
+  // set1 有依赖其他的 任务 set2 被其他依赖的 任务
+  const [startPoints] = getSets(inputs);
+  // 首先将 开头的元素加入 res
+  res.push(...startPoints);
   for (let i = 0; i < inputs.length; i++) {
     const [next, prev] = inputs[i].split("->");
-    if(i === 0) {
-      res.push(prev, next);
-    }else {
-      if (res.includes(next) && !res.includes(prev)) {
-        // 需要将 prev 插入到 next 前面
-        const index = res.indexOf(next);
-        res.splice(index, 0, prev);
-      }else if(res.includes(prev) && !res.includes(next) ){
-        // 直接追加到 尾部
-        !res.includes(next) && res.push(next);
-      } else {
-        !res.includes(prev) && res.push(prev);
-        !res.includes(next) && res.push(next);
-      }
+    if (res.includes(next) && !res.includes(prev)) {
+      // 需要将 prev 插入到 next 前面
+      const index = res.indexOf(next);
+      res.splice(index, 0, prev);
+    }else if(res.includes(prev) && !res.includes(next) ){
+      // 直接追加到 尾部
+      res.push(next);
+    } else {
+      !res.includes(prev) && res.push(prev);
+      !res.includes(next) && res.push(next);
     }
+
   }
   console.log(res)
   return res;
 }
 
 appendTo(['A->B', 'B->D', 'D->E', 'E->G', 'G->C']);
-
 appendTo(['A->B', 'C->B']);
 appendTo(['B->A', 'C->A', 'D->B', 'D->C', 'D->E']);
+appendTo(['B->C', 'A->C', 'D->E']); // C E A B D
 
 
 
