e8c92fc048fb5a735dd26328a190ac3e3583302f
caoguangyao <caoguangyao28@163.com>
2024-11-30 21:49:45 +0800

    feat: todoiife-demo代码

---
+++ js/iife.js
@@ -0,0 +1,20 @@
+/**
+ * iife 利用立即执行函数优化函数执行
+ *
+ *
+ */
+(function () {
+  // 私有变量
+  var name = 'iife'
+  // 私有方法
+  function showName() {
+    console.log(name)
+  }
+  // 公开方法
+  window.iife = {
+    showName
+  }
+})();
+
+// 利用iife 提炼 环境判断 不同环境 执行不同函数
+// todo iife
